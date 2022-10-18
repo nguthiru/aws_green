b29f6c2f3ae76c33a1589add87144b61c2c039b1
Edwin Ndiritu <eddiendiritu@gmail.com>
2022-10-18 13:47:55 +0300

    static fix

--- greendays/settings.py
+++ greendays/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_ACCESS_KEY_ID = config.get("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = config.get("AWS_S3_SECRET_ACCESS_KEY")
